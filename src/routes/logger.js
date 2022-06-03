7b0e7f32a41bdb0e7f5b0b2b74ef342d72bfb2e1
rakeshdeyol <rakeshdeyol868@gmail.com>
2022-06-03 14:27:27 +0530

    adding nodejs assignment

--- src/routes/logger.js
+++ src/routes/logger.js
@@ -1,6 +1,8 @@
 // const endpoint = "https://www.functionup.org"
 // const batch = "radon"
 
+const { log } = require("../logger/logger");
+
 // const log = function() {
 //     console.log('I am inside the log function')
 // }
@@ -10,3 +12,47 @@
 // module.exports.log = log
 
 
+const chunk = function(){
+    const months = ['jan','feb','mar','april','may','june','july','aug','sep','oct','nov','dec'];
+    console.log(months.slice(0,4));
+    console.log(months.slice(4,8));
+    console.log(months.slice(8,12));
+}
+
+const tail = function(){
+    let arrayOfOddNumbers = [1,3,5,7,9,11,13,15,17,19,21];
+    console.log(arrayOfOddNumbers.slice(1,10));
+}
+
+
+const union = function(){
+    let arr1 = [1,2,3,4];
+    let arr2 = [5,6,7,4];
+    let arr3 = [11,12,9,5];
+    let arr4 = [14,23,33,12];
+    let arr5 = [12,22,23,4];
+
+    let newarr = arr1.concat(arr2,arr3,arr4,arr5);
+    let uniqueArr =[... new Set(newarr)];
+    console.log(uniqueArr);
+}
+
+let fromPairs = function(){
+    let array = []
+    var arrayList={
+        thor : "the shining",
+        drama :"titanic",
+        thriller:"shutter island",
+        fantasy:"hero"
+    }
+
+    for(let key in arrayList){
+        array.push([key,arrayList[key]])
+    }
+    console.log(array);
+}
+
+module.exports.chunk = chunk;
+module.exports.tail = tail;
+module.exports.union = union;
+module.exports.fromPairs = fromPairs;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,13 @@
 const express = require('express');
-<<<<<<< Updated upstream
 
-const externalModule = require('../logger/logger')
-const helperModule = require("../util/helper.js")
-const formatterModule = require("../validator/formatter.js")
-=======
-const externalModule = require('./logger')
->>>>>>> Stashed changes
+
+//const externalModule = require('../logger/logger')
+//const helperModule = require("../util/helper.js")
+//const formatterModule = require("../validator/formatter.js")
+//const externalModule = require('./logger')
+
+const mylogger = require('./logger')
+
 
 const router = express.Router();
 
@@ -17,41 +18,15 @@ const router = express.Router();
 //     res.send('My first ever api!')
 // });
 
-router.get('/test-me', function (req, res) {
-<<<<<<< Updated upstream
-    externalModule.log()
-    console.log("todays date (dd/mm) - " +helperModule.dd() + "/" +helperModule.mm())
-    helperModule.batchInfo()
-    console.log(`i apply trim - ${formatterModule.trim}`)
-    formatterModule.lower()
-    formatterModule.upper()
-    res.send('My first ever api!')
+router.get('/hello', function (req, res) {
+    mylogger.chunk();
+    mylogger.tail();
+    mylogger.union();
+    mylogger.fromPairs();
+
+    res.send('My first  api!')
+    
 });
 
 module.exports= router ;
-=======
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
-});
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
-module.exports = router;
-// adding this comment for no reason
->>>>>>> Stashed changes
