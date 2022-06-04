b8cdf9794e89d8fb38f589845dd8ddbe37a5373c
rakeshdeyol <rakeshdeyol868@gmail.com>
2022-06-04 18:34:36 +0530

    submit the class assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -31,6 +31,52 @@ router.get('/hello1', function (req, res) {
     res.send('Hello there!')
 });
 
+//Q1.
+// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+// Your route code will look like this
+router.get('/sol1', function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    let missingNumber
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]+1 != arr[i+1]){
+            missingNumber= arr[i]+1;
+
+        }
+        
+    }
+
+    ///LOGIC WILL GO HERE 
+    res.send({ data: missingNumber  })
+
+
+});
+
+
+
+//Q2. 
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+// Your route code will look like this
+router.get("/sol2", function (req, res) {
+        //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+        let arr= [33, 34, 35, 37, 38]
+        let missingNumber1
+
+        for(let i=0;i<arr.length-1;i++){
+            if(arr[i]+1 != arr[i+1]){
+                missingNumber1= arr[i]+1;
+    
+            }
+            
+        }
+
+        ///LOGIC WILL GO HERE 
+
+        res.send(  { data: missingNumber1  }  );
+});
+
+
+
 router.get('/candidates', function(req, res){
     console.log('Query paramters for this request are '+JSON.stringify(req.query))
     let gender = req.query.gender
