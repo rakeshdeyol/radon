349773e1683de5f1e3f4552d0935bab6324ba6c8
rakeshdeyol <rakeshdeyol868@gmail.com>
2022-06-15 02:01:50 +0530

    adding the source code

--- src/index.js
+++ src/index.js
@@ -7,17 +7,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose
+  .connect(
+    'mongodb+srv://rakeshdeyol:rakeshdeyol@cluster0.qjgmo.mongodb.net/rakeshdeyol-db?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log('MongoDb is connected'))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+app.use(function (req, res, next) {
+  let token = req.headers['x-Auth-token'];
+  if (!token) {
+    token = req.headers['x-auth-token'];
+    if (!token)
+      return res.send({ status: false, msg: 'token must be present' });
+    else {
+      next();
+    }
+  }
+});
 
-
-app.use('/', route)
-
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log('Express app running on port ' + (process.env.PORT || 3000));
 });
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController = require('../controllers/userController');
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.get('/test-me', function (req, res) {
+  res.send('My first ever api!');
+});
 
-router.post("/users", userController.createUser  )
+router.post('/users', userController.createUser);
 
-router.post("/login", userController.loginUser)
+router.post('/login', userController.loginUser);
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get('/users/:userId', userController.getUserData);
 
-router.put("/users/:userId", userController.updateUser)
+router.put('/users/:userId', userController.updateUser);
+
+router.delete('/users/:userId', userController.deleteUser);
 
 module.exports = router;
